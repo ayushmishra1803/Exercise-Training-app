742a17d7865447b088f0809f2b38baa8ea6e4917
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-05-08 10:50:40 +0530

    updates

---
+++ src/app/classes/TrainingService/training-service.spec.ts
@@ -0,0 +1,7 @@
+import { TrainingService } from './training-service';
+
+describe('TrainingService', () => {
+  it('should create an instance', () => {
+    expect(new TrainingService()).toBeTruthy();
+  });
+});
