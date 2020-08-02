7e6ddb845377047a21502a2cb2442dadcb822f85
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-03 01:15:06 +0530

    Updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyDxeO9Kdv3vbhLOEA8S1oXdIuOYPiFnnF8",
-    authDomain: "ng-fitness-tracker-eb57c.firebaseapp.com",
-    databaseURL: "https://ng-fitness-tracker-eb57c.firebaseio.com",
-    projectId: "ng-fitness-tracker-eb57c",
-    storageBucket: "ng-fitness-tracker-eb57c.appspot.com",
-    messagingSenderId: "141640990554",
-    appId: "1:141640990554:web:73625a2b0547e25b00fd63",
-    measurementId: "G-S5LQWQ1JDV"
-  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,16 +5,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyDxeO9Kdv3vbhLOEA8S1oXdIuOYPiFnnF8",
-    authDomain: "ng-fitness-tracker-eb57c.firebaseapp.com",
-    databaseURL: "https://ng-fitness-tracker-eb57c.firebaseio.com",
-    projectId: "ng-fitness-tracker-eb57c",
-    storageBucket: "ng-fitness-tracker-eb57c.appspot.com",
-    messagingSenderId: "141640990554",
-    appId: "1:141640990554:web:73625a2b0547e25b00fd63",
-    measurementId: "G-S5LQWQ1JDV"
-  }
+
 };
 
 /*
